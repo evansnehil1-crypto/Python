1c95a9bb0e43203f63c3497bac47db1f7db18b93
evansnehil1-crypto <evansnehil1@gmail.com>
2026-07-06 16:24:11 +0200

    Add files via upload

---
+++ storing birthdays.py
@@ -0,0 +1,21 @@
+family1 = input("Enter family member's name: ")
+birthday1 = input("Enter their birthday: ")
+
+family2 = input("Enter family member's name: ")
+birthday2 = input("Enter their birthday: ")
+
+family3 = input("Enter family member's name: ")
+birthday3 = input("Enter their birthday: ")
+
+family4 = input("Enter family member's name: ")
+birthday4 = input("Enter their birthday: ")
+
+family5 = input("Enter family member's name: ")
+birthday5 = input("Enter their birthday: ")
+
+print("\nFamily Birthdays:")
+print(family1, "-", birthday1)
+print(family2, "-", birthday2)
+print(family3, "-", birthday3)
+print(family4, "-", birthday4)
+print(family5, "-", birthday5)
