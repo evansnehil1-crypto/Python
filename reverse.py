df68ee764055bb467ff51e58fb83095936e0a3e6
evansnehil1-crypto <evansnehil1@gmail.com>
2026-07-22 16:20:18 +0200

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,10 @@
+string = input("Please enter your own STRING: ")
+
+string2 = ('')
+
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String = ", string)
+print("The reversed string = ", string2)
+
