ef2858d7d0a19d1e09148ced6d25cab7d05ce34f
evansnehil1-crypto <evansnehil1@gmail.com>
2026-08-25 16:18:26 +0200

    Update function calculator.py

--- function calculator.py
+++ function calculator.py
@@ -11,36 +11,34 @@ def divide(a, b):
     return a / b
 
 print("===== Function Calculator =====")
-print("Add")
-print("Subtract")
-print("Multiply")
-print("Divide")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
 
 try:
-
-    choice = input("Choose a operation (+, -, * or /): ")
+    choice = input("Choose an operation (1, 2, 3, 4): ")
 
     num1 = float(input("Enter first number: "))
-
     num2 = float(input("Enter second number: "))
 
-    if choice == "+":
+    if choice == "1":
         result = add(num1, num2)
-
-    elif choice == "-":
+    elif choice == "2":
         result = subtract(num1, num2)
-
-    elif choice == "*":
+    elif choice == "3":
         result = multiply(num1, num2)
-
-    elif choice == "/":
+    elif choice == "4":
         result = divide(num1, num2)
-
     else:
         print("Invalid operation")
+        result = None
+
+    if result is not None:
+        print("Result:", result)
 
 except ValueError:
     print("Please enter numbers only")
 
 except ZeroDivisionError:
-    print("You cannto divide by zero")
+    print("Cannot divide by zero")
