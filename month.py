47c4ffa7092cc461c871953edce3d48eb8b4d23d
evansnehil1-crypto <evansnehil1@gmail.com>
2026-08-31 15:19:22 +0200

    Add files via upload

---
+++ month.py
@@ -0,0 +1,14 @@
+import calendar
+
+print(calendar.month_name[1])
+print(calendar.month_name[2])
+print(calendar.month_name[3])
+print(calendar.month_name[4])
+print(calendar.month_name[5])
+print(calendar.month_name[6])
+print(calendar.month_name[7])
+print(calendar.month_name[8])
+print(calendar.month_name[9])
+print(calendar.month_name[10])
+print(calendar.month_name[11])
+print(calendar.month_name[12])
